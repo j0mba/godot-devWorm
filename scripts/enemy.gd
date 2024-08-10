504bf0d973d5647eb5efeadbf66b3059d708c113
j0mba <sousa.sje@gmail.com>
2024-08-10 08:53:47 -0300

    first commit

---
+++ scripts/enemy.gd
@@ -0,0 +1,57 @@
+extends CharacterBody2D
+
+var speed = 40
+var player_chase = false
+var player = null
+
+var health = 100
+var player_in_attack_zone = false
+var can_take_damage = true
+
+func _physics_process(delta):
+	deal_with_damage()
+	
+	if player_chase:
+		position += (player.position - position)/speed
+		
+		$AnimatedSprite2D.play("side_walk")
+		if(player.position.x - position.x) < 0 :
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.play("front_idle")
+
+func _on_detection_area_body_entered(body):
+	player = body
+	player_chase = true
+
+
+func _on_detection_area_body_exited(body):
+	player = null
+	player_chase = false
+	
+func enemy():
+	pass
+
+func _on_enemy_hit_box_body_entered(body):
+	if body.has_method("player"):
+		player_in_attack_zone = true
+
+
+func _on_enemy_hit_box_body_exited(body):
+	if body.has_method("player"):
+		player_in_attack_zone = false
+		
+func deal_with_damage():
+	if player_in_attack_zone and global.player_current_attack == true:
+		if can_take_damage:
+			$TakeDamageColdown.start()
+			can_take_damage = false
+			health -= 20
+			if health <= 0:
+				self.queue_free()
+
+
+func _on_take_damage_coldown_timeout():
+	can_take_damage = true
---
+++ scripts/player.gd
@@ -0,0 +1,147 @@
+extends CharacterBody2D
+
+var enemy_in_attack_range = false
+var enemy_attack_coldown = true
+var health = 200
+var player_alive = true
+
+const SPEED = 100.0
+var current_dir = "down"
+var attack_in_progress = false
+
+func _ready():
+	play_animation()
+
+func _physics_process(delta):
+	attack()
+	handle_input()
+	move_and_slide()
+	enemy_attack()
+	curent_camera()
+	
+	
+	if health <= 0:
+		player_alive = false
+		health = 0
+		self.queue_free()
+
+func handle_input():
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	
+	# Normaliza a velocidade para garantir que a velocidade seja constante
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * SPEED
+	
+	update_direction()
+	play_animation()
+
+
+
+func update_direction():
+	if velocity.x > 0:
+		current_dir = "right"
+	elif velocity.x < 0:
+		current_dir = "left"
+	elif velocity.y > 0:
+		current_dir = "down"
+	elif velocity.y < 0:
+		current_dir = "up"
+
+func play_animation():
+	# Se um ataque está em progresso, mantenha a animação atual
+	if attack_in_progress:
+		return
+
+	var anim = $AnimatedSprite2D
+	match current_dir:
+		"right":
+			anim.flip_h = false
+			if velocity.x != 0 or velocity.y != 0:
+				anim.play("side_walk")
+			else:
+				anim.play("side_idle")
+		"left":
+			anim.flip_h = true
+			if velocity.x != 0 or velocity.y != 0:
+				anim.play("side_walk")
+			else:
+				anim.play("side_idle")
+		"down":
+			if velocity.y != 0 or velocity.x != 0:
+				anim.play("front_walk")
+			else:
+				anim.play("front_idle")
+		"up":
+			if velocity.y != 0 or velocity.x != 0:
+				anim.play("back_walk")
+			else:
+				anim.play("back_idle")
+
+
+func player():
+	pass
+
+func _on_player_hit_box_body_entered(body):
+	if body.has_method("enemy"):
+		enemy_in_attack_range = true
+
+func _on_player_hit_box_body_exited(body):
+	if body.has_method("enemy"):
+		enemy_in_attack_range = false
+		
+func enemy_attack():
+	if enemy_in_attack_range and enemy_attack_coldown == true:
+		health -= 20
+		enemy_attack_coldown = false
+		$AttackColdown.start()
+
+func _on_attack_coldown_timeout():
+	enemy_attack_coldown = true
+
+func attack():
+	# Verifique se o ataque está em progresso
+	if attack_in_progress:
+		return  # Evita iniciar outro ataque se um já estiver em progresso
+	
+	# Define a direção do ataque
+	var dir = current_dir
+	if Input.is_action_just_pressed("attack"):
+		global.player_current_attack = true
+		attack_in_progress = true
+		if dir == "right":
+			$AnimatedSprite2D.flip_h = false
+			$AnimatedSprite2D.play("side_attack")
+		elif dir == "left":
+			$AnimatedSprite2D.flip_h = true
+			$AnimatedSprite2D.play("side_attack")
+		elif dir == "down":
+			$AnimatedSprite2D.play("front_attack")
+		elif dir == "up":
+			$AnimatedSprite2D.play("back_attack")
+		
+		$DealAttackTimer.start()
+			
+
+func _on_deal_attack_timer_timeout():
+	$DealAttackTimer.stop()
+	global.player_current_attack = false
+	attack_in_progress = false
+	
+	# Após o ataque, continue a permitir o movimento
+	play_animation()
+
+func curent_camera():
+	if global.current_scene == "world":
+		$WorldCamera.enabled = true
+		$CliffSideCamera.enabled = false
+	elif global.current_scene =="cliff_side":
+		$WorldCamera.enabled = false
+		$CliffSideCamera.enabled = true
